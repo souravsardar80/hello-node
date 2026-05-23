f560de46455fb78a79b413933e840f541e5c9700
souravsardar80 <souravsardar@aol.com>
2026-05-23 19:15:03 +0530

    reading file with fs module
    first data read then convert string from buffer using utf-8 and
    toString().Then for browser readabilty returned json and in productController stringified

--- node_file_structure/src/controller/product.controller.ts
+++ node_file_structure/src/controller/product.controller.ts
@@ -1,14 +1,19 @@
 import type { IncomingMessage, ServerResponse } from "node:http";
+import { readProduct } from "../service/productService";
 
 export const productController = (req: IncomingMessage, res: ServerResponse)=>{
     const url = req.url;
     const method = req.method;
-    const products = [
-        {
-            id: 1,
-            name:   "Product1"
-        }
-    ]
+    // const products = [
+    //     {
+    //         id: 1,
+    //         name:   "Product1"
+    //     }
+    // ]
+
+    // readProduct();
+
+    const products = readProduct();
 
     if(url === "/products" && method === "GET"){
           res.writeHead(200,{"content-type" : "application/json"});
---
+++ node_file_structure/src/service/productService.ts
@@ -0,0 +1,18 @@
+import path from "node:path";
+import fs from "fs";
+
+const filePath = path.join(process.cwd(),"/src/database/db.json")
+
+export const readProduct = () => {
+    // console.log(process.cwd());
+    // console.log(filePath);
+    // const products = fs.readFileSync(filePath);
+    // console.log(products.toString());
+
+    const products = fs.readFileSync(filePath, "utf-8");
+
+    // console.log(products);
+    // return products;
+        console.log(JSON.parse(products));
+    return JSON.parse(products);
+}
