8d36b7fb1682f63036ac8bdfcc95f21226fc829e
souravsardar80 <souravsardar@aol.com>
2026-05-29 12:39:56 +0530

    parsing req body

--- node_file_structure/src/controller/product.controller.ts
+++ node_file_structure/src/controller/product.controller.ts
@@ -1,40 +1,58 @@
 import type { IncomingMessage, ServerResponse } from "node:http";
 import { readProduct } from "../service/productService";
 import type { IProduct } from "../types/product.type";
+import { parseBody } from "../utility/parseBody";
 
-export const productController = (req: IncomingMessage, res: ServerResponse)=>{
-    const url = req.url;
-    const method = req.method;
+export const productController = async (
+  req: IncomingMessage,
+  res: ServerResponse,
+) => {
+  // console.log("Request", req);
+  const url = req.url;
+  const method = req.method;
 
-    // const products = [
-    //     {
-    //         id: 1,
-    //         name:   "Product1"
-    //     }
-    // ]
+  // const products = [
+  //     {
+  //         id: 1,
+  //         name:   "Product1"
+  //     }
+  // ]
 
-    // readProduct();
+  // readProduct();
 
-    const urlParts = url?.split('/');
-    // console.log(urlParts);
-    const id = urlParts && urlParts[1] === "products" ? Number(urlParts[2]) : null;
+  const urlParts = url?.split("/");
+  // console.log(urlParts);
+  const id =
+    urlParts && urlParts[1] === "products" ? Number(urlParts[2]) : null;
 
-    // console.log(`the product id is: ${id}`);
+  // console.log(`the product id is: ${id}`);
 
-    const products: IProduct = readProduct();
+  const products: IProduct = readProduct();
 
-    if(url === "/products" && method === "GET"){
-          res.writeHead(200,{"content-type" : "application/json"});
-        res.end(JSON.stringify({message:"This is products route",
-         data: products   
-        }));
-    } else if(method === "GET" && id !== null) {
-        const product = products.find((p: IProduct) => p.id === id);
-        // console.log(product);
-        res.writeHead(200,{"content-type" : "application/json"});
-        res.end(JSON.stringify({message:"Product retrived scuccessfully",
-         data: product   
-        }));
-    }
-  
-}
+  if (url === "/products" && method === "GET") {
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(
+      JSON.stringify({ message: "This is products route", data: products }),
+    );
+  } else if (method === "GET" && id !== null) {
+    const product = products.find((p: IProduct) => p.id === id);
+    // console.log(product);
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(
+      JSON.stringify({
+        message: "Product retrived scuccessfully",
+        data: product,
+      }),
+    );
+  } else if (method === "POST" && url === "/products") {
+    const body = await parseBody(req);
+    console.log("Body", body);
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(
+      JSON.stringify({
+        message: "Product retrived scuccessfully",
+        // data: "",
+      }),
+    );
+  }
+};
